48d9fd88877765f79e0e0fabc85d8601722d1c1b
ldoolan <ldoolan@paradigm-healthcare.com>
2014-02-04 14:44:42 -0800

    add a few delete statements

--- Oracle/data_fixes/SHN/sp_visits/show.sql
+++ Oracle/data_fixes/SHN/sp_visits/show.sql
@@ -9,9 +9,12 @@ set pages 100
 
 define spk_list=603068,603084,602636,602638,602640,602694,603754,603760,605952,605578,605592,605446,604988,604236
 
+-- delete from nursing.sp_visits where sp_visit_id in (&spk_list)
+
 select
    n.sp_visit_id,
    to_char(n.visit_date, 'yyyy-mm-dd') visit_date,
+   n.comments comments,
    s.fname   sfn,
    s.lname   sln,
    n.empid,
@@ -25,4 +28,5 @@ where  1=1
    and n.sp_visit_id in (&spk_list)
    and s.student_id=n.sid
    and u.userid=n.empid
-/
+
+-- delete from nursing.sp_visits where sp_visit_id in (&spk_list)
