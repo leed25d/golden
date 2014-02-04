939f39963b9c4810eff8f1e7f71e7bd2348156f6
ldoolan <ldoolan@paradigm-healthcare.com>
2014-02-04 14:46:15 -0800

    output formatting + select attributes

--- Oracle/data_fixes/SHN/treatments/d_treatments.sql
+++ Oracle/data_fixes/SHN/treatments/d_treatments.sql
@@ -1,8 +1,14 @@
-define trids = 7494188
-select treatment_id, outcome, service_date, minutes_type, minutes, billed
+
+set lines 220
+set pages 60
+
+define trids = 11476762,11476820,11476908,11476952
+
+select treatment_id, plan, outcome, service_date, minutes_type, minutes, billed
 from para.treatments where treatment_id in (&trids)
 /
 
+--  update para.treatments set plan='NONE' where treatment_id in (&trids)
 --  update para.treatments set outcome='absent' where treatment_id in (&trids)
 --  delete from para.treatments where treatment_id in (&trids)
 
