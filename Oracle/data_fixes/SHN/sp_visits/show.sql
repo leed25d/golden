dad58a54567e95147618fd3470b887c06c8116a4
ldoolan <ldoolan@paradigm-healthcare.com>
2014-02-04 14:43:28 -0800

    page formatting

--- Oracle/data_fixes/SHN/sp_visits/show.sql
+++ Oracle/data_fixes/SHN/sp_visits/show.sql
@@ -1,10 +1,11 @@
 col sfn format a15
 col sln format a25
 col visit_date format a12
+col comments format a20 wrap
 col ufn format a15
 col uln format a25
-set lines 120
-set pages 60
+set lines 180
+set pages 100
 
 define spk_list=603068,603084,602636,602638,602640,602694,603754,603760,605952,605578,605592,605446,604988,604236
 
@@ -25,4 +26,3 @@ where  1=1
    and s.student_id=n.sid
    and u.userid=n.empid
 /
-
